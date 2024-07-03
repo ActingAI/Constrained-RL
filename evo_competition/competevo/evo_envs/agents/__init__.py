bac2ab66100652a12612aebec5b10a7c0d444a6b
kjaebye <kangyao.huang@outlook.com>
2024-07-03 16:08:07 +0800

    [rearrange]

--- evo_competition/competevo/evo_envs/agents/__init__.py
+++
@@ -1,8 +0,0 @@
-from .dev_ant import DevAnt
-from .dev_ant_fighter import DevAntFighter
-
-from .dev_bug import DevBug
-from .dev_bug_fighter import DevBugFighter
-
-from .dev_spider import DevSpider
-from .dev_spider_fighter import DevSpiderFighter
---
+++ evo_gym/dev_envs/agents/__init__.py
@@ -0,0 +1,3 @@
+from .dev_ant import DevAnt
+from .dev_bug import DevBug
+from .dev_spider import DevSpider
